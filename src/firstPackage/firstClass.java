fa29ff21fd1df0dbee688fb1112e45fede3f357f
Sabinya Somadas <43927559+sabinyasomadas@users.noreply.github.com>
2018-10-08 20:23:31 -0700

    Update firstClass.java

--- src/firstPackage/firstClass.java
+++ src/firstPackage/firstClass.java
@@ -36,7 +36,7 @@ public class firstClass {
 		is.next();
 		
 		driver.switchTo().window(is.next());
-		
+		System.out.println(driver.getTitle());
 		driver.switchTo().defaultContent();
 		System.out.println(driver.getTitle());
 		
